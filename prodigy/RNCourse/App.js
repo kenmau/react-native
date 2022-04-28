8b6fc88629aec90d6cc4bdc1e522b454c14f4491
Ken Mau <ken.mau@prodigygame.com>
2022-04-28 16:08:13 -0400

    Udemy React Native Practical Guide 2022: Chapter 34

---
+++ prodigy/RNCourse/App.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+  Modal,
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function addGoalHandler(enteredGoalText) {
+    // new state depends on previous state, better practice is to pass a function
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { id: Math.random().toString(), text: enteredGoalText },
+    ]);
+    console.log(enteredGoalText);
+    endAddGoalHandler();
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      // return a new array minus all the items you filter out
+      // filter() itself take a function which has to return true/false
+      // if the evaluation is false, then the item is removed.
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+    console.log("DELETE");
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <Button
+        title="Add New Goal"
+        color="#5e0acc"
+        onPress={startAddGoalHandler}
+      ></Button>
+
+      <GoalInput
+        visible={modalIsVisible}
+        onAddGoal={addGoalHandler}
+        onCancel={endAddGoalHandler}
+      />
+
+      <View style={styles.goalsContainer}>
+        {/* FlatList is more efficient and renders cells only when needed. Other items are loaded lazily */}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical="false"
+        ></FlatList>
+
+        {/* ScrollView renders everything all the time */}
+        {/* <ScrollView alwaysBounceVertical="false">
+          {courseGoals.map((goal) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text key={goal} style={styles.goalText}>
+                  {goal}
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView> */}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+});
